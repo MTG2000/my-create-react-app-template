a77e35ee51c704a720b7da1307395f8a4a505a1c
= <=>
2019-06-14 13:55:02 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../style/style.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h2>This is Main App</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
